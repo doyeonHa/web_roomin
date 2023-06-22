e0b9a48429b9d3f2da1fc544c37839fe82e56e7a
doyeonHa <w2113@e-mirim.hs.kr>
2023-06-23 03:16:13 +0900

    초기

--- src/App.js
+++ src/App.js
@@ -2,70 +2,17 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from './navbar/index';
 import Main from "./pages/Mainstart";
-import Wepcam from "./pages/Filming";
-import ChoiceImg from "./pages/ChoiceImg"
-import WebcamApp from "./pages/WebcamApp"
-import WebcamApp2 from "./pages/WebcamApp2"
-import Frame from "./pages/Frame"
-import ChoiceFrame from "./pages/Choiceframe"
-import ChoiceFrame2 from "./pages/Choiceframe2"
-import Write from "./pages/Write"
-import Write2 from "./pages/Write2"
-import Test from "./pages/Test"
-import Test2 from "./pages/Test2"
-import Gallery from "./pages/Gallery"
-import SearchSave from "./pages/SearchSave"
-import Loding from "./pages/Loding"
-import Result from "./pages/Result"
-import Result2 from "./pages/Result2"
+import Signup from "./pages/Signuping"
+import Reserve from "./pages/Reserve"
+import Sidenav from './sidenav/Sidenav';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/photoshoot" element={
-            <div> <Navbar /> <WebcamApp /></div>
-          }/>
-        <Route path="/photoshoot2" element={ 
-            <div> <Navbar /> <WebcamApp2 /></div>
-          }/>
-        <Route path="/choiceimg" element={ 
-            <div> <Navbar /> <ChoiceImg /></div>
-          }/>
-        <Route path="/frame" element={ 
-            <div> <Navbar /> <Frame /></div>
-          }/>
-        <Route path="/ChoiceFrame" element={ 
-            <div> <Navbar /> <ChoiceFrame /></div>
-          }/>
-        <Route path="/ChoiceFrame2" element={ 
-            <div> <Navbar /> <ChoiceFrame2 /></div>
-          }/>
-        <Route path="/Write" element={ 
-            <div> <Navbar /> <Write /></div>
-          }/>
-        <Route path="/Write2" element={ 
-            <div> <Navbar /> <Write2 /></div>
-          }/>
-        <Route path="/Gallery" element={ 
-            <div> <Navbar /> <Gallery /></div>
-          }/>
-        <Route path="/SearchSave" element={ 
-            <div> <Navbar /> <SearchSave /></div>
-          }/>
-        <Route path="/Loding" element={ 
-            <div> <Navbar /> <Loding /></div>
-          }/>
-        <Route path="/Result" element={ 
-            <div> <Navbar /> <Result /></div>
-          }/>
-        <Route path="/Result2" element={ 
-            <div> <Navbar /> <Result2 /></div>
-          }/>
-        <Route path="/filming" element={<Wepcam />} />
-        <Route path="/Test" element={<Test />} />
-        <Route path="/Test2" element={<Test2 />} />
+        <Route path="/" element={<div> <Navbar /> <Main /></div>} />
+        <Route path="/Signup" element={ <div> <Navbar /> <Signup /></div>}/>
+        <Route path="/Reserve" element={ <div> <Sidenav/> <Reserve /></div>}/>
       </Routes>
     </Router>
   );
--- src/pages/Mainstart.jsx
+++ src/pages/Mainstart.jsx
@@ -1,130 +1,82 @@
-import React from 'react';
-import logo from '../img/frameloginsmall.png';
+import React, {useState, useSession, useRouter} from 'react';
 import styled from 'styled-components';
 import { NavLink as Link } from 'react-router-dom';
-import { FaBars } from 'react-icons/fa';
-// import image from '../img/framelogo'
-import image2 from '../img/whitelogo.png'
-import mainimg from '../img/backimg.png'
 import { useNavigate } from "react-router-dom";
+import './MainStart.css'
+import image from '../image/roomin.png';
+import axios from 'axios'
 
 function Mainstart(){
-  const navigate = useNavigate();
-  localStorage.clear();
-  function handleClick() {
-     navigate("/frame");
+  const [id, setId] = useState("");
+  const [pw, setPw] = useState(""); 
+
+  const handleIdChange = (e) => {
+    setId(e.target.value);
+  };
+
+  const handlePwChange = (e) => {
+    setPw(e.target.value);
+  };
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      // 서버로 로그인 요청을 보냄
+      const response = await axios.post("/api/login", {
+        email: id,
+        password: pw
+      });
+  
+      // 응답의 상태 코드를 확인하여 로그인 성공 여부를 판단
+      if (response.status === 200) {
+        const user = response.data.user;
+        const message = response.data.message;
+  
+        alert(message); // 로그인 성공 출력
+        // router.replace('/');
+      } else {
+        // 로그인 실패 처리
+        alert('로그인 실패: 이메일 또는 비밀번호가 잘못되었습니다.');
+      }
+    } catch (error) {
+      console.error("로그인 중 오류 발생:", error.message);
+    }
      
   }
    return (
-	  <body style={{height: '100%', width: '100%',position: 'absolute', backgroundSize: 'cover', backgroundImage: `url(${mainimg})`}}>
-<Container>
-		 <Nav>
-			<Bars />
-			<NavMenu>
-			   <Logo to='/'>
-               <LogoImage src={image2} alt="logo" />
-			   </Logo>
-			   <Blank/>
-			   		<Wrapper>
-							<Links href="/frame">PHOTO</Links>
-							<Links href="/Gallery">네컷갤러리</Links>
-		            </Wrapper>
-			   </NavMenu>
-		 </Nav>
-	  </Container>
-		 <div id="backdrop">
-				<div id="logo">
-					<img src={image2} width="275" height="125" alt="logo"/>
-				</div>
-				<div id="btnSt" onClick={handleClick}>사진 촬영하기</div>
-			</div>
+	  <body>
+      <Logo style={{backgroundImage: `url(${image})`}}></Logo>
+      <Form id="forms" method="post">
+        <input type="text" placeholder="호텔 아이디" class="input1" id="inp1" name="id"/>
+        <input type="password" placeholder="비밀번호" class="input2" id="inp2" name="pw"/>
+      </Form>
+      <div id="btnsignup">
+        <div id="pass" class="sign">비밀번호 찾기</div>
+        <div id="line" class="sign"> | </div>
+        <div id="signup" class="sign" onclick="window.location.href='/회원가입/html/step1.html'">회원가입</div>
+      </div>
+      <div id="mark"></div>
+      <button type="submit" id="loginbtn" onclick="window.location.href = './관리자페이지/main.html'">로그인</button>
 	  </body>
    );
 }
-const Container = styled.div`
-  height: 8%;
-  width: 98%;
-  margin: 0 auto;
-`;
-const Wrapper = styled.div`
-  display: flex;
-  justify-content: space-evenly;
-  align-items: center;
-  column-gap: 3%;
-  color: white;
-  font-size: 1.5vw;
-  width: 22%;
+const Form = styled.div`
+  position: absolute;
+  width: 508px;
+  height: 180px;
+  left: 1186px;
+  top: 403px;
+  display: grid;
+  grid-template-rows: 80px 80px;
 `;
 
-const Links = styled.a`
-  font-family: "Noto Serif";
-  text-decoration: none;
-  color: white;
-  font-size: 0.7vw;
-  width: 27%;
-  display: block;
-  text-align: center;
-  border-radius: 2vw;
-  background-blend-mode: overlay;
-  background: linear-gradient(297.97deg, rgba(255, 255, 255, 0.15) 6.99%, rgba(255, 255, 255, 0.35) 94.43%);
-   box-shadow: 0px 4px 36px rgba(0, 0, 0, 0.05);
-   backdrop-filter: blur(15px);
-  padding: 0.2vw 0.1vw;
-    &:hover {
-      transition: all 0.3s;
-      border: 0.2vw solid #abbf98;
-    }
-`;
-
-const Nav = styled.nav`
-  position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
-  width: 100vw;
-  height: 8%;
-  display: flex;
-  justify-content: flex-end;
-  margin: 0 auto;
-  z-index: 12;
-`;
-
-const Logo = styled(Link)`
-  color: #000000;
-  text-decoration: none;
-  margin-top: 1%;
-  width: 5%;
-  height: auto;
-`;
-
-const LogoImage = styled.img`
-  width: 100%;
-  height: 100%;
-  padding: 1%;
-`;
-
-const Blank = styled.div`
+const Logo = styled.div`
+  position: absolute;
+  width: 210.71px;
+  height: 40px;
+  left: 1335px;
+  top: 233px;
+  background-size: cover;
 `
-const NavMenu = styled.div`
-		display: flex;
-    justify-content: space-evenly;
-    -webkit-box-align: center;
-    align-items: center;
-    width: 100%;
-    column-gap: 19%;
-`;
 
-const Bars = styled(FaBars)`
-  display: none;
-  color: #808080;
-  @media screen and (max-width: 768px) {
-    display: block;
-    position: absolute;
-    top: 0;
-    right: 0;
-    transform: translate(-100%, 75%);
-    font-size: 1.8rem;
-    cursor: pointer;
-  }
-`;
 export default Mainstart;
