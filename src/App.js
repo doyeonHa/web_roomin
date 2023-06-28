ac5c7a5fac23bc039572d196d8cd3ca72d07002d
doyeonHa <w2113@e-mirim.hs.kr>
2023-06-29 00:01:43 +0900

    App.js

--- src/App.js
+++ src/App.js
@@ -3,8 +3,11 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from './navbar/index';
 import Main from "./pages/Mainstart";
 import Signup from "./pages/Signuping"
-import Reserve from "./pages/Reserve"
 import Sidenav from './sidenav/Sidenav';
+import Reserve from "./pages/Reserve";
+import Check from "./pages/Checkinout";
+import Service from "./pages/FreeCharge";
+import Detail from "./pages/Detail";
 
 function App() {
   return (
@@ -12,7 +15,10 @@ function App() {
       <Routes>
         <Route path="/" element={<div> <Navbar /> <Main /></div>} />
         <Route path="/Signup" element={ <div> <Navbar /> <Signup /></div>}/>
-        <Route path="/Reserve" element={ <div> <Sidenav/> <Reserve /></div>}/>
+        <Route path="/Reserve" element={ <div> <Sidenav /> <Reserve /></div>}/>
+        <Route path="/Check" element={ <div> <Sidenav props={1}/> <Check /></div>}/>
+        <Route path="/Service" element={ <div> <Sidenav /> <Service /></div>}/>
+        <Route path="/Detail" element={ <div> <Sidenav /> <Detail /></div>}/>
       </Routes>
     </Router>
   );
